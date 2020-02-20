ebbe97c66025a9343b2f9ac503e5ba224d6e9f4a
Xuejie Xiao <xxuejie@gmail.com>
2020-02-20 05:52:45 +0000

    Cosmetic fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const { Molecule } = require('molecule-javascript')
 const schema = require('../schema/blockchain-combined.json')
 
-const scriptTypeIndex = schema.declarations.map(declaration => declaration.name).indexOf('Script')
+const names = schema.declarations.map(declaration => declaration.name)
+const scriptTypeIndex = names.indexOf('Script')
 const scriptType = schema.declarations[scriptTypeIndex]
 
 // Write your script logic here.
