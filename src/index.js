8de23513fbffc0f126ddc381816bf8e6584359cf
Xuejie Xiao <xxuejie@gmail.com>
2020-03-12 06:06:44 +0000

    Rebuild template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-const { Molecule } = require('molecule-javascript')
-const schema = require('../schema/blockchain-combined.json')
-
-const names = schema.declarations.map(declaration => declaration.name)
-const scriptTypeIndex = names.indexOf('Script')
-const scriptType = schema.declarations[scriptTypeIndex]
+// Include CKB data structures using the following style import
+// import { Script } from "../schema/blockchain.js";
 
 // Write your script logic here.
-CKB.debug(scriptType)
+CKB.debug("I'm running in CKB!")
